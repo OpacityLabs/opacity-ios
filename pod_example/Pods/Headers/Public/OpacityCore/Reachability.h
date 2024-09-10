7bf9729090a7ada7a0b77a9ddd560363a2c63beb
Oscar Franco <ospfranco@gmail.com>
2024-09-10 16:41:00 -0400

    Delete committed pods

--- pod_example/Pods/Headers/Public/OpacityCore/Reachability.h
+++
@@ -1,1 +0,0 @@
-../../../../../Opacity/Reachability.h
--- pod_example/Pods/Target Support Files/Pods-Opacity_Example/Pods-Opacity_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Opacity_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Opacity_Example
-@end
